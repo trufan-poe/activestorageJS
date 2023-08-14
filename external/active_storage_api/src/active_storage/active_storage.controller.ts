9a88287a079f7fa4353fbac64a477bf28d970fd3
Poe <poe@trufan.io>
2023-08-14 11:00:17 -0400

    fix test app routing and param parsing.

--- external/active_storage_api/src/active_storage/active_storage.controller.ts
+++ external/active_storage_api/src/active_storage/active_storage.controller.ts
@@ -1,18 +1,21 @@
 import { Controller, Get, Res, HttpStatus, Param } from '@nestjs/common';
 import { Response } from 'express';
 import { ActiveStorageJS } from 'activestorageJS';
-@Controller('active-storage')
+@Controller('active_storage')
 export class ActiveStorageController {
   @Get(':blob_name')
-  findAll(@Param('blob_name') blobName: string, @Res({ passthrough: true }) res: Response) {
+  findAll(
+    @Param('blob_name') blobName: string,
+    @Res({ passthrough: true }) res: Response,
+  ) {
     res.status(HttpStatus.OK);
     return {
       url: new ActiveStorageJS().service().url(blobName, {
-        disposition: 'attachment',
+        disposition: 'inline',
         filename: 'something.png',
         contentType: 'image/png',
-        tokenDuration: null
-      })
+        tokenDuration: null,
+      }),
     };
   }
 }
--- external/active_storage_api/src/show/show.controller.ts
+++ external/active_storage_api/src/show/show.controller.ts
@@ -2,16 +2,24 @@ import { Controller, Get, Res, Req, HttpStatus, Param } from '@nestjs/common';
 import { Response, Request } from 'express';
 import { ActiveStorageJS } from 'activestorageJS';
 import { JwtService } from '@nestjs/jwt';
-@Controller('show')
+@Controller('active_storage/disk')
 export class ShowController {
   @Get(':token/:filename')
-  async findAll(@Res() res: Response, @Req() req: Request) {
+  async findAll(
+    @Res() res: Response,
+    @Req() req: Request,
+    @Param('token') token,
+  ) {
     try {
-      const newToken = req.originalUrl.split('/')[2];
-      const validated = new JwtService().verify(Buffer.from(newToken, 'base64').toString(), {
-        publicKey: process.env.JWT_SECRET
-      });
-      const file: Buffer = await new ActiveStorageJS().service().download(validated['key']);
+      const validated = new JwtService().verify(
+        Buffer.from(token, 'base64').toString(),
+        {
+          publicKey: process.env.JWT_SECRET,
+        },
+      );
+      const file: Buffer = await new ActiveStorageJS()
+        .service()
+        .download(validated['key']);
       res.status(HttpStatus.OK);
       res.setHeader('Content-Type', validated['content_type']);
       res.setHeader('Content-Disposition', 'inline');
